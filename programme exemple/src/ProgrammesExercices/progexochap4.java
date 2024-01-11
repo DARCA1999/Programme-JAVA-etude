f83ef36618a0ef9891a2c1d45766736428344fe2
BRYAN <bryanpaguy@gmail.com>
2024-01-11 16:07:30 -0500

    version 2

--- programme exemple/src/ProgrammesExercices/progexochap4.java
+++ programme exemple/src/ProgrammesExercices/progexochap4.java
@@ -17,30 +17,30 @@ public class progexochap4 {
         Scanner lectureClavier = new Scanner(System.in);
         System.out.println("Pour obtenir un cafe, entrez au moins 45 cents");
         System.out.println("je rends la monnaie ");
-        do
-        {
-            System.out.println("valeur de la pièce entree : ");
-            pièce = lectureClavier.nextInt();
-
-            switch (pièce)
-            {
-                case 5 : nbPièce5c = nbPièce5c + 1 ;
-                break;
-                case 10 : nbPièce10c = nbPièce10c + 1 ;
-                break;
-                case 20 : nbpièce20c = nbpièce20c + 1 ;
-                break;
-                default:
-                    pièce = 0;
-                    System.out.println("pièce impossible");
-            }
-            totalRecu = totalRecu + pièce ;
-            System.out.println("Vous avez entre : ");
-            System.out.println("    " + nbPièce5c + "pièce de 5 cents");
-            System.out.println("    " + nbPièce10c + "pièce de 10 cents");
-            System.out.println("    " + nbpièce20c + "pièce de 5 cents");
-
-        }
+//        do
+//        {
+//            System.out.println("valeur de la pièce entree : ");
+//            pièce = lectureClavier.nextInt();
+//
+//            switch (pièce)
+//            {
+//                case 5 : nbPièce5c = nbPièce5c + 1 ;
+//                break;
+//                case 10 : nbPièce10c = nbPièce10c + 1 ;
+//                break;
+//                case 20 : nbpièce20c = nbpièce20c + 1 ;
+//                break;
+//                default:
+//                    pièce = 0;
+//                    System.out.println("pièce impossible");
+//            }
+//            totalRecu = totalRecu + pièce ;
+//            System.out.println("Vous avez entre : ");
+//            System.out.println("    " + nbPièce5c + "pièce de 5 cents");
+//            System.out.println("    " + nbPièce10c + "pièce de 10 cents");
+//            System.out.println("    " + nbpièce20c + "pièce de 5 cents");
+//
+//        }
 
     }
 }
